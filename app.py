ef0233bea14f696d13f6e9fad62d6b0fbb4290f5
liupeng413 <liupbroccoli@gmail.com>
2025-12-01 03:02:38 +0800

    fix

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def handle_message(data):
     emit('new_message', {'username': username, 'content': content}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
 
